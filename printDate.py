b21240ab59f0aa9a278c42af0edd4e8dcea3c76c
Amit Bhadoria <amitme0014@gmail.com>
2018-10-31 20:31:10 +0530

    Create printDate.py

---
+++ printDate.py
@@ -0,0 +1,7 @@
+#This code prints date,day,month and year using datetime module
+
+import datetime
+print(datetime.date.today())
+print(datetime.date.today().day)
+print(datetime.date.today().month)
+print(datetime.date.today().year)
